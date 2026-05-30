b7fdddae8faecd8592f31b94c9b994954de7f17b
greninjam26 <94926874+greninjam26@users.noreply.github.com>
2026-05-29 20:48:08 -0400

    connected create new tour to db

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,4 +1,4 @@
-const fs = require("fs");
+// const fs = require("fs");
 const Tour = require("../models/tourModel");
 
 // const tours = JSON.parse(
@@ -15,16 +15,6 @@ const Tour = require("../models/tourModel");
 //   }
 //   next();
 // };
-
-exports.checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
-    return res.status(400).json({
-      status: "fail",
-      message: "Missing name or price",
-    });
-  }
-  next();
-};
 
 // this function is usually called the route handler
 exports.getAllTours = (req, res) => {
@@ -52,8 +42,13 @@ exports.getTour = (req, res) => {
   });
 };
 
-exports.createTour = (req, res) => {
-  // res.status(201).json({ status: "success", data: { tour: newTour } });
+exports.createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
+    res.status(201).json({ status: "success", data: { tour: newTour } });
+  } catch (err) {
+    res.status(400).json({ status: "fail", message: err });
+  }
 };
 
 exports.updateTour = (req, res) => {};
